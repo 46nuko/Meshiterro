c96cd1febb853c98c9eb06b8764f055b6d3e30bf
46nuko <152859182+46nuko@users.noreply.github.com>
2026-04-25 05:44:43 +0000

    CRUD機能全実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-
+  
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  has_many :post_images, dependent: :destroy
+
 end
